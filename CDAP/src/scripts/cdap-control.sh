45dc5555f3ddebf5a6ee95fa5bcb4b1d8de98ff3
dereklwood <derek@continuuity.com>
2014-12-12 15:55:04 -0800

    preserving work

--- CDAP/src/scripts/cdap-control.sh
+++ CDAP/src/scripts/cdap-control.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # expects arguments [package] [service] [action]
 PACKAGE=$1
 SERVICE=$2
 CMD=$3
+
+echo "PWD: `pwd`"
+
+# source common functions from CDAP init framework to reuse here
+source $CDAP_HOME/$PACKAGE/bin/common.sh
 
 MY_ENV=`env`
 
@@ -12,16 +17,82 @@ echo $MY_ENV
 echo "*******************************************"
 echo "ZOOKEEPER? $ZK_QUORUM"
 
+HBASE_CLASSPATH=`hbase classpath`
+echo "**************** HBASE_CLASSPATH **************"
+echo $HBASE_CLASSPATH
+echo "***********************************************"
+
+echo "*************** substituting zookeeper"
+
+perl -pi -e "s#{{ZK_QUORUM}}#${ZK_QUORUM}#" ${CONF_DIR}/cdap-site.xml
+echo "done"
+
+
 # set the conf dir to cloudera managers agent directory for this process
 export CDAP_CONF=$CONF_DIR
 
-case $CMD in
-  (start)
-    echo "Starting the CDAP ${SERVICE} service"
-    echo "CDAP_HOME: ${CDAP_HOME}"
-    exec $CDAP_HOME/$PACKAGE/bin/service $CMD $SERVICE
-    ;;
-  (*)
-    echo "Don't understand [$CMD]"
-    ;;
-esac
+export CDAP_COMPONENT_HOME=${CDAP_HOME}/${PACKAGE}
+
+# pull in CDH environment vars
+export HADOOP_HOME=$CDH_HADOOP_HOME
+export HBASE_HOME=$CDH_HBASE_HOME
+export YARN_HOME=$CDH_YARN_HOME
+
+
+echo "*** attempting to source ${CDAP_HOME}/${PACKAGE}/conf/${SERVICE}-env.sh"
+# source the service-specific conf to get $MAIN_CLASS or $MAIN_CMD
+if [ -f ${CDAP_HOME}/${PACKAGE}/conf/${SERVICE}-env.sh ]; then
+  . ${CDAP_HOME}/${PACKAGE}/conf/${SERVICE}-env.sh
+fi
+
+echo "MAIN_CLASS: $MAIN_CLASS"
+echo "MAIN_CMD: $MAIN_CMD"
+
+
+if [ $MAIN_CLASS ]; then
+  # launch a java app
+
+  # set options
+  export OPTS="-XX:+UseConcMarkSweepGC"
+
+  # set java
+  JAVA=$JAVA_HOME/bin/java
+
+  set_classpath $CDAP_COMPONENT_HOME $CDAP_CONF
+
+  # Setup hive classpath if hive is installed, this has to be run after HBASE_CP is setup by set_classpath.
+  set_hive_classpath
+
+  set_hbase
+
+  echo "`date` Starting Java service $SERVICE on `hostname`"
+  "$JAVA" -version
+  echo "`ulimit -a`"
+
+  exec "$JAVA" -Dcdap.service=$SERVICE "$JAVA_HEAPMAX" \
+    -Dexplore.conf.files=$EXPLORE_CONF_FILES \
+    -Dexplore.classpath=$EXPLORE_CLASSPATH "$OPTS" \
+    -cp $CLASSPATH $MAIN_CLASS $MAIN_CLASS_ARGS 
+
+elif [ $MAIN_CMD ]; then
+  # launch a non-java app
+
+  echo "`date` Starting service $SERVICE on `hostname`"
+  echo "`ulimit -a`"
+
+  exec $MAIN_CMD $MAIN_CMD_ARGS
+
+fi
+
+
+
+#case $CMD in
+#  (start)
+#    echo "Starting the CDAP ${SERVICE} service"
+#    echo "CDAP_HOME: ${CDAP_HOME}"
+#    exec $CDAP_HOME/$PACKAGE/bin/service $CMD $SERVICE
+#    ;;
+#  (*)
+#    echo "Don't understand [$CMD]"
+#    ;;
+#esac
