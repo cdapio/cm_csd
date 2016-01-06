abf61e751d51e223b2457c1b3a6e7db76af2407a
Chris Gianelloni <chris@cask.co>
2016-01-06 10:48:35 -0500

    Updated with PR feedback

--- src/scripts/cdap-control.sh
+++ src/scripts/cdap-control.sh
@@ -110,7 +110,7 @@ source ${COMPONENT_HOME}/bin/common-env.sh
 source ${COMPONENT_HOME}/bin/common.sh
 
 # Remap CDAP common init functions, if necessary
-fn_exists() { type ${1} | grep -q 'shell function'; }
+fn_exists() { type -t ${1} | grep -q function; }
 
 fn_exists cdap_set_classpath || cdap_set_classpath() { set_classpath; }
 fn_exists cdap_set_hive_classpath || cdap_set_hive_classpath() { set_hive_classpath; }
